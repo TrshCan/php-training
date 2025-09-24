6fbe2a177df6846e6ffe9a6e39970e391196a924
TrshCan <phuthuytinhyeu1234678@gmail.com>
2025-09-24 21:22:24 +0700

    removed redis. Implement session-based CSRF tokens and secure forms (login, logout, add/edit, delete)

--- 11-training-php/login.php
+++ 11-training-php/login.php
@@ -5,26 +5,31 @@ session_start();
 require_once 'models/UserModel.php';
 $userModel = new UserModel();
 
+// Generate CSRF token if not already present
+if (empty($_SESSION['csrf_token'])) {
+    $_SESSION['csrf_token'] = bin2hex(random_bytes(32));
+}
 
 if (!empty($_POST['submit'])) {
+    if (!isset($_POST['csrf_token']) || $_POST['csrf_token'] !== $_SESSION['csrf_token']) {
+        die("CSRF validation failed");
+    }
+
     $users = [
         'username' => $_POST['username'],
         'password' => $_POST['password']
     ];
-    $user = NULL;
-    if ($user = $userModel->auth($users['username'], $users['password'])) {
-        //Login successful
-        $_SESSION['id'] = $user[0]['id'];
 
+    if ($user = $userModel->auth($users['username'], $users['password'])) {
+        // Login successful
+        $_SESSION['id'] = $user[0]['id'];
         $_SESSION['message'] = 'Login successful';
         header('location: list_users.php');
-    }else {
-        //Login failed
+        exit;
+    } else {
         $_SESSION['message'] = 'Login failed';
     }
-
 }
-
 ?>
 <!DOCTYPE html>
 <html>
@@ -35,53 +40,49 @@ if (!empty($_POST['submit'])) {
 <body>
 <?php include 'views/header.php'?>
 
-    <div class="container">
-        <div id="loginbox" style="margin-top:50px;" class="mainbox col-md-6 col-md-offset-3 col-sm-8 col-sm-offset-2">
-            <div class="panel panel-info" >
-                <div class="panel-heading">
-                    <div class="panel-title">Login</div>
-                    <div style="float:right; font-size: 80%; position: relative; top:-10px"><a href="#">Forgot password?</a></div>
-                </div>
+<div class="container">
+    <div id="loginbox" style="margin-top:50px;" class="mainbox col-md-6 col-md-offset-3 col-sm-8 col-sm-offset-2">
+        <div class="panel panel-info">
+            <div class="panel-heading">
+                <div class="panel-title">Login</div>
+                <div style="float:right; font-size: 80%; position: relative; top:-10px"><a href="#">Forgot password?</a></div>
+            </div>
+            <div style="padding-top:30px" class="panel-body">
+                <form method="post" class="form-horizontal" role="form">
+                    <input type="hidden" name="csrf_token" value="<?php echo htmlspecialchars($_SESSION['csrf_token']); ?>">
 
-                <div style="padding-top:30px" class="panel-body" >
-                    <form method="post" class="form-horizontal" role="form">
+                    <div class="margin-bottom-25 input-group">
+                        <span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span>
+                        <input id="login-username" type="text" class="form-control" name="username" placeholder="username or email">
+                    </div>
 
-                        <div class="margin-bottom-25 input-group">
-                            <span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span>
-                            <input id="login-username" type="text" class="form-control" name="username" value="" placeholder="username or email">
+                    <div class="margin-bottom-25 input-group">
+                        <span class="input-group-addon"><i class="glyphicon glyphicon-lock"></i></span>
+                        <input id="login-password" type="password" class="form-control" name="password" placeholder="password">
+                    </div>
+
+                    <div class="margin-bottom-25">
+                        <input type="checkbox" name="remember" id="remember">
+                        <label for="remember"> Remember Me</label>
+                    </div>
+
+                    <div class="margin-bottom-25 input-group">
+                        <div class="col-sm-12 controls">
+                            <button type="submit" name="submit" value="submit" class="btn btn-primary">Submit</button>
                         </div>
+                    </div>
 
-                        <div class="margin-bottom-25 input-group">
-                            <span class="input-group-addon"><i class="glyphicon glyphicon-lock"></i></span>
-                            <input id="login-password" type="password" class="form-control" name="password" placeholder="password">
+                    <div class="form-group">
+                        <div class="col-md-12 control">
+                            Don't have an account!
+                            <a href="form_user.php">Sign Up Here</a>
                         </div>
-
-                        <div class="margin-bottom-25">
-                            <input type="checkbox" tabindex="3" class="" name="remember" id="remember">
-                            <label for="remember"> Remember Me</label>
-                        </div>
-
-                        <div class="margin-bottom-25 input-group">
-                            <!-- Button -->
-                            <div class="col-sm-12 controls">
-                                <button type="submit" name="submit" value="submit" class="btn btn-primary">Submit</button>
-                                <a id="btn-fblogin" href="#" class="btn btn-primary">Login with Facebook</a>
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            <div class="col-md-12 control">
-                                    Don't have an account!
-                                    <a href="form_user.php">
-                                        Sign Up Here
-                                    </a>
-                            </div>
-                        </div>
-                    </form>
-                </div>
+                    </div>
+                </form>
             </div>
         </div>
     </div>
+</div>
 
 </body>
 </html>
--- 11-training-php/logout.php
+++ 11-training-php/logout.php
@@ -1,5 +1,29 @@
 <?php
 session_start();
-session_destroy();
-header('location: login.php');
+
+// Generate CSRF token if not present
+if (empty($_SESSION['csrf_token'])) {
+    $_SESSION['csrf_token'] = bin2hex(random_bytes(32));
+}
+
+// If POST request, process logout
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    if (!isset($_POST['csrf_token']) || $_POST['csrf_token'] !== $_SESSION['csrf_token']) {
+        die("CSRF validation failed");
+    }
+
+    session_destroy();
+    header('location: login.php');
+    exit;
+}
 ?>
+<!DOCTYPE html>
+<html>
+<head><title>Logout</title></head>
+<body>
+    <form method="POST" action="logout.php">
+        <input type="hidden" name="csrf_token" value="<?php echo htmlspecialchars($_SESSION['csrf_token']); ?>">
+        <button type="submit" class="btn btn-danger">Logout</button>
+    </form>
+</body>
+</html>
